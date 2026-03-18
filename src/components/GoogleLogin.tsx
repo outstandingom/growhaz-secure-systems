50d0e4e96bbc76d9c105ff6921353b6728a719c9
Om Uikey <ouikey41@gmail.com>
2026-03-19 01:31:14 +0530

    Create GoogleLogin.tsx

---
+++ src/components/GoogleLogin.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react"
+import { useGoogleAuth } from "../hooks/useGoogleAuth"
+
+const GoogleLogin: FC = () => {
+
+  const { signInWithGoogle } = useGoogleAuth()
+
+  return (
+    <button
+      onClick={signInWithGoogle}
+      style={{
+        padding: "10px 16px",
+        borderRadius: "6px",
+        border: "1px solid #ddd",
+        cursor: "pointer"
+      }}
+    >
+      Continue with Google
+    </button>
+  )
+}
+
+export default GoogleLogin
