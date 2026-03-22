0cf1e34287280711ea3f8402e7e5849613cf4cf8
Om Uikey <ouikey41@gmail.com>
2026-03-22 12:17:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 // Page Imports
 
 import Index from "./pages/Index";
