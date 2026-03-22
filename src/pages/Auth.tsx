2fa40bf3d85d23996ecfd995ea151141582675ef
Om Uikey <ouikey41@gmail.com>
2026-03-22 12:30:59 +0530

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 import { Layout } from "@/components/layout/Layout";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+
 import { Label } from "@/components/ui/label";
 import {
   Dialog,
